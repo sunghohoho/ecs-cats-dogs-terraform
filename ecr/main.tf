2b82e7f9123115fa71a3fdd4b5ba49e788502f88
sunghohoho <kim5259746@gmail.com>
2024-03-05 18:44:36 +0900

    add 'web ecr'
    web 이미지를 담을 ecr 추가

--- ecr/main.tf
+++ ecr/main.tf
@@ -60,3 +60,32 @@ module "public_ecr_dogs" {
   }
 }
 
+module "public_ecr_webs" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "${var.project_name}-webs"
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Terraform   = "true"
+    Environment = "${var.project_name}"
+    Repo = "webs"
+  }
+}
