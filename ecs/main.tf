e8ff5b1aa37c0487ca483a6e3c34711335972036
sunghohoho <kim5259746@gmail.com>
2024-03-09 15:21:59 +0900

    add 'web def'
    web definition 추가

--- ecs/main.tf
+++ ecs/main.tf
@@ -11,6 +11,21 @@ locals {
   env = "terraform"
 }
 
+module "webs_task_def" {
+  source = "../module/ecs/task_definition"
+  is_fargate = false
+  family = "${var.project_name}-webs"
+  cpu = "1024"
+  mem = "2048"
+  container_cpu = "512"
+  container_mem = "1024"
+  network_mode = "bridge"
+  container_name = "cats"
+  container_url = "${data.terraform_remote_state.ecr.outputs.webs-ecr}"
+  containerport = "80"
+  hostport = "80"
+}
+
 module "cats_task_def" {
   source = "../module/ecs/task_definition"
   is_fargate = false
