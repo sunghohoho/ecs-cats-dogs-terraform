dac32869e952e8af7b113ab4813f8eec2a1d787f
sunghohoho <kim5259746@gmail.com>
2024-03-09 17:18:34 +0900

    add '입력 변수 추가'
    ㅈㄱㄴ

--- module/ecs/cluster/variables.tf
+++ module/ecs/cluster/variables.tf
@@ -2,3 +2,29 @@ variable "project_name" {
   description = "클러스터 이름"
   type        = string
 }
+
+variable "is_ec2_provider" {
+  description = "클러스터 이름"
+  type        = bool
+}
+
+variable "subnet_id" {
+  description = "subnet_id"
+  type        = list(string)
+}
+
+variable "max_size" {
+  description = "인스턴스 최대 갯수"
+  type        = number
+}
+
+variable "min_size" {
+  description = "인스턴스 최소 갯수"
+  type        = number
+}
+
+variable "desire_size" {
+  description = "인스턴스 희망 값"
+  type        = number
+}
+
