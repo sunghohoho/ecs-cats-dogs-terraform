b7d77d9952d2042e082ff0144b166e15faebbabf
sunghohoho <kim5259746@gmail.com>
2024-03-12 17:41:52 +0900

    update 'security group'
    security group 모듈 사용하여 2개 sg생성

--- sg/main.tf
+++ sg/main.tf
@@ -9,27 +9,44 @@ data "terraform_remote_state" "vpc" {
 module "ecs-svc-alb-sg" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "web-server"
-  description = "Security group for web-server with HTTP ports open within VPC"
+  name        = "ecs-svc-alb-sg"
+  description = "ecs service alb securirty group, 80 access"
 #   vpc_id      = data.terraform_remote_state.vpc.vpc_id
   vpc_id = "vpc-2332ab48"
 
-  ingress_cidr_blocks      = ["10.10.0.0/16"]
-  ingress_rules            = ["https-443-tcp"]
+  ingress_cidr_blocks      = ["0.0.0.0/0"]
+  ingress_rules            = ["http"]
   ingress_with_cidr_blocks = [
     {
-      from_port   = 8080
-      to_port     = 8090
+      from_port   = 80
+      to_port     = 80
       protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    },
-    {
-      rule        = "postgresql-tcp"
-      cidr_blocks = "0.0.0.0/0"
+      description = "@@@@@@@@@@User-service ports@@@@@@"
+      cidr_blocks = "0.0.0.0/16"
     },
   ]
 }
 
+module "ecs-instance-sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "ecs-instance-sg"
+  description = "ecs capacity provider ec2 instance security group"
+#   vpc_id      = data.terraform_remote_state.vpc.vpc_id
+  vpc_id = "vpc-2332ab48"
+
+  ingress_cidr_blocks      = ["0.0.0.0/0"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 65535
+      protocol    = "tcp"
+      description = "@@@@@@@@@@User-service ports@@@@@@"
+      cidr_blocks = "10.10.0.0/16"
+    },
+
+  ]
+}
 
 
