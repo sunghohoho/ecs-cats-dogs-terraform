05829e9d4f2bc2263fb8df93d199b40475fdfd68
sunghohoho <kim5259746@gmail.com>
2024-04-17 11:21:21 +0900

    add 'acm trf import 내용 추가'
    ㅈㄱㄴ

--- acm/main.tf
+++ acm/main.tf
@@ -8,6 +8,15 @@
 #   }
 # }
 
+#######################
+# terraform import 과정
+# 1. 더미 resource 생성
+# 2. terraform import 'aws_acm_ceriticate.cert' arn:aws:acm:us-east-1:866477832211:certificate/f651979e-5410-4021-85ce-44ccebb0685a 입력
+# 3. terraform state list를 통해 state 상태 확인
+# 4. tfstate 또는 terraform show를 통해서 가져온 리소스 state 내용 확인
+# 5. 수동으로 resource 생성, 필요없는 arg 삭제 후 plan을 통해 diff가 없는 것 확인
+#######################
+
 resource "aws_acm_certificate" "cert" {
     #arn                       = "arn:aws:acm:us-east-1:866477832211:certificate/f651979e-5410-4021-85ce-44ccebb0685a"
     domain_name               = "*.gguduck.com"
